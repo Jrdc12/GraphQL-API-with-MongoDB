3d8bf21b970d20e1eb4f315f24a3cb28c21b6ec8
personal <juliodc289@gmail.com>
2023-03-14 16:05:27 -0400

    hopefully this works...

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-require("dotenv").config()
-const { ApolloServer } = require("apollo-server")
-const mongoose = require("mongoose")
+require("dotenv").config();
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const mongoose = require("mongoose");
 const { merge } = require("lodash");
 
-const mongoString = process.env.DATABASE_URL
+const mongoString = process.env.DATABASE_URL;
 
 // Apollo Server
 // typeDefs: GraphQL Type Definitions
 // resolvers: How do we resolve queries and mutations
 
-const employeeTypeDefs = require("./graphql/employeeTypeDefs")
-const employeeResolvers = require("./graphql/employeeResolvers")
-const userTypeDefs = require("./graphql/userTypeDefs")
-const userResolvers = require("./graphql/userResolver")
+const employeeTypeDefs = require("./graphql/employeeTypeDefs");
+const employeeResolvers = require("./graphql/employeeResolvers");
+const userTypeDefs = require("./graphql/userTypeDefs");
+const userResolvers = require("./graphql/userResolver");
 
-const server = new ApolloServer({
-  typeDefs: [employeeTypeDefs, userTypeDefs],
-  resolvers: merge(employeeResolvers, userResolvers),
-})
+async function startServer() {
+  const server = new ApolloServer({
+    typeDefs: [employeeTypeDefs, userTypeDefs],
+    resolvers: merge(employeeResolvers, userResolvers),
+  });
 
-mongoose
-  .connect(mongoString, { useNewUrlParser: true })
-  .then(() => {
-    console.log("MongoDB Connected")
-    return server.listen({ port: 8080 })
-  })
-  .then((res) => {
-    console.log(`Server running at ${res.url}`)
-  })
+  await server.start();
+
+  const app = express();
+  server.applyMiddleware({ app });
+
+  await mongoose.connect(mongoString, { useNewUrlParser: true });
+  console.log("MongoDB Connected");
+
+  app.listen({ port: 8080 }, () =>
+    console.log(`Server running at http://localhost:8080${server.graphqlPath}`)
+  );
+}
+
+startServer();
